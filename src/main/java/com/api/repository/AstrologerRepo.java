202fc3951e08fda7831a6939d9e1e65b4437b438
tannumodanwal <tannumodanwal09@gmail.com>
2024-12-13 00:39:36 +0530

    Add files via upload

---
+++ src/main/java/com/api/repository/AstrologerRepo.java
@@ -0,0 +1,9 @@
+package com.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.entities.Astrologer;
+
+public interface AstrologerRepo extends JpaRepository<Astrologer,Long>{
+
+}
