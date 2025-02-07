c0ca2503edbe9b1b2ae11b5cd054b75e90e20bb7
tannumodanwal <tannumodanwal09@gmail.com>
2025-02-07 17:16:21 +0530

    Add files via upload

---
+++ src/main/java/com/api/exception/CustomNotFoundException.java
@@ -0,0 +1,8 @@
+package com.api.exceptions;
+
+
+public class CustomNotFoundException extends RuntimeException{
+	  public CustomNotFoundException(String message) {
+	        super(message);
+	  }
+}
