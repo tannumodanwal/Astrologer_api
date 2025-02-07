3f508123c8911fd71457cb40777eff1973b5e948
tannumodanwal <tannumodanwal09@gmail.com>
2025-02-07 17:15:00 +0530

    Add files via upload

--- src/main/java/com/api/enums/CommunicationType.java
+++ src/main/java/com/api/enums/CommunicationType.java
@@ -1,1 +1,12 @@
+package com.api.enums;
 
+
+
+public enum CommunicationType {
+
+	VIDEO_CALL,
+    AUDIO_CALL,
+    CHAT
+	
+	
+}
---
+++ src/main/java/com/api/enums/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.api.enums;
+
+
+
+
+public enum OrderStatus{
+
+	PENDING,
+    COMPLETED,
+    CANCELLED
+	
+}
