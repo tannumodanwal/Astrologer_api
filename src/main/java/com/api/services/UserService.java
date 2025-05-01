b5fcf6c43ef737c7309db8f5af7fb23be9cda759
tannumodanwal <tannumodanwal09@gmail.com>
2025-05-01 23:56:50 +0530

    Update UserService.java

--- src/main/java/com/api/services/UserService.java
+++ src/main/java/com/api/services/UserService.java
@@ -1,23 +1,22 @@
 package com.api.services;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.api.entities.User;
 
-
 @Service
 public interface UserService {
-
-	public List<User> getAllUsers();
-	
+    public List<User> getAllUsers();	
 	public User getById(Integer userId);
-	
-	public String addUser(User user);
-	
-	public String updateUser(User user);
-	
-	public String deleteById(Integer userId);
-	
+	public User addUser(User user);	
+	public User updateUser(Integer userId,User user);	
+	public String deleteById(Integer userId);	
+	public User getUserByEmail(String email);
+	public User loginUser(String email, String password);	
+	public User changePassword(String email, String oldPassword, String newPassword);
+	void uploadUserImage(Integer userId, MultipartFile imageFile) throws IOException;
 }
