f5467c0fd3852ac81cc8f82669a98846e8127feb
tannumodanwal <tannumodanwal09@gmail.com>
2025-05-01 23:52:10 +0530

    Update UserRepo.java

--- src/main/java/com/api/repository/UserRepo.java
+++ src/main/java/com/api/repository/UserRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.api.entities.User;
 
 public interface UserRepo extends JpaRepository<User,Integer>{
-
+      Optional<User> findByEmail(String email); 
 }
