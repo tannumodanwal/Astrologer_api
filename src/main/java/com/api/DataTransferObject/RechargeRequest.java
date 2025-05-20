a969992138a24daf6c715fb99a5814e8ac9db833
tannumodanwal <tannumodanwal09@gmail.com>
2025-05-20 20:19:34 +0530

    Add files via upload

---
+++ src/main/java/com/api/DataTransferObject/RechargeRequest.java
@@ -0,0 +1,24 @@
+package com.api.DTO;
+
+public class RechargeRequest {
+	
+	    private int userId;
+	    private double amount;
+	    
+	    
+		public int getUserId() {
+			return userId;
+		}
+		public void setUserId(int userId) {
+			this.userId = userId;
+		}
+		public double getAmount() {
+			return amount;
+		}
+		public void setAmount(double amount) {
+			this.amount = amount;
+		}
+	    
+	    
+
+}
